48d8c04410f9925d59da6cea219dad322879e947
TheDevKiller <thedevkiller@protonmail.com>
2019-04-17 19:27:41 +0200

    fix: get arguments in URL (download.js)

--- download.js
+++ download.js
@@ -1,7 +1,7 @@
 let url = window.location.href;
 
 if (url.startsWith("https://chrome.google.com/webstore/detail/")) {
-    let id = /https:\/\/chrome\.google\.com\/webstore\/detail\/.+\/(.+)/.exec(url)[1];
+    let id = /https:\/\/chrome\.google\.com\/webstore\/detail\/.+\/(\w+)/.exec(url)[1];
     let version = /Chrome\/(\d+\.\d+\.\d+\.\d+)/.exec(navigator.userAgent)[1];
     window.open(`https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&prodversion=${version}&x=id%3D${id}%26installsource%3Dondemand%26uc`)
 }
